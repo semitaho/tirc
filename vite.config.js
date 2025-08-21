4fa92b07e67909701747f00f80fd2984eabdf528
Toni Aho <toni@tahoo.fi>
2025-08-21 12:29:20 +0300

    alles gut

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  clean: true,
   root: 'app',
   plugins: [react()],
     base: '/tirc/',  // 👈 must match repo name
